4fce5ec027600b60b1fc56515f4cf1227dbab11d
Pablo - Note +A <pquiroga@maisaedu.com.br>
2024-08-22 07:33:14 -0300

    finalizado

--- js/app.js
+++ js/app.js
@@ -2,22 +2,22 @@ const comprar = () => {
 
     // DOM: tipo de ingresso
     const ingressoEscolhidoInput = document.querySelector('#tipo-ingresso').value;
-    
+
     // DOM: quantidade de ingresso para comprar
     const ingressoEscolhidoQuantidadeInput = parseInt(document.querySelector('#qtd').value);
-    
+
     // DOM: quantidade de ingresso disponível
-    let ingressoEscolhidoQuantidadeDisponivel = document.querySelector(`#qtd-${ingressoEscolhidoInput}`).innerHTML
-    
+    let ingressoEscolhidoQuantidadeDisponivel = document.querySelector(`#qtd-${ingressoEscolhidoInput}`);
+
     console.log(ingressoEscolhidoQuantidadeDisponivel)
 
-    if (isNaN(ingressoEscolhidoQuantidadeInput) || ingressoEscolhidoQuantidadeInput > ingressoEscolhidoQuantidadeDisponivel) {
+    if (isNaN(ingressoEscolhidoQuantidadeInput) || ingressoEscolhidoQuantidadeInput > parseInt(ingressoEscolhidoQuantidadeDisponivel.innerHTML)) {
 
         alert('A quantidade de compra precisa ser um número menor ou igual a quantidade disponível.');
 
     } else {
 
-       ingressoEscolhidoQuantidadeDisponivel = "kkkkk"
+        ingressoEscolhidoQuantidadeDisponivel.innerHTML = parseInt(ingressoEscolhidoQuantidadeDisponivel.innerHTML) - parseInt(ingressoEscolhidoQuantidadeInput)
 
     }
 
